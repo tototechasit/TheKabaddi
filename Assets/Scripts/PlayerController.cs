f9c2bd13f94355c227ecdcacc7a57d8a4a6b6a0e
TechasitA <techasit.a@ku.th>
2019-08-05 11:08:10 +0700

    Change Joystick to be  asset joystick.
    Change Joystick to be  asset joystick. Player movement system is complete now.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private static Rigidbody rb;
-    public static float speed = 10;
-    private static Transform player;
+    private static Rigidbody rb;        // It's player movement component.
+    public static float speed = 10;     // Fixed value for control player movement speed.
+    private static Transform player;    // Player object.
+
+    protected Joystick joystick;        // Game controller.
 
 
 	void Start () {
         rb = GetComponent<Rigidbody>();
         player = GetComponent<Transform>();
+
+        joystick = FindObjectOfType<Joystick>();
 	}
-	
-	//void FixedUpdate () {
- //       float horizontalMove = Input.GetAxis("Horizontal");
- //       float verticalMove = Input.GetAxis("Vertical");
 
- //       Vector3 movement = new Vector3(horizontalMove, 0.0f, verticalMove);
+    // Get movement command from Joystick.
+    void FixedUpdate() {
+        float horizontalMove = joystick.Horizontal;
+        float verticalMove = joystick.Vertical;
 
- //       moveCharacter(movement);
- //   }
+        Vector3 movement = new Vector3(horizontalMove, 0, verticalMove);
 
+        moveCharacter(movement);
+    }
+
+    // Move player.
     public static void moveCharacter(Vector3 movement) {
         rb.velocity = movement * speed;
-
-
-        //player.Translate(movement * speed);
-
-        //Debug.Log("player: " + player.position.x + ", " + player.position.y + ", " + player.position.z + ", ");
     }
 }
